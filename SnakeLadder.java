6aa4ec418d109f0809dc4eaba13f8ba9ae0a568a
rakibbiswas1 <rakibbiswas.official@gmail.com>
2022-08-19 03:21:14 +0530

    Game start with single player and position is 0

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,6 +1,13 @@
 
 public class SnakeLadder {
-	public static void main(String[] args) {
-        System.out.println("Welcome to Snake and Ladder game");
+	
+    	public static final int POSITION = 0;
+    	
+    	public static void main(String args[]) {
+
+    		System.out.println("Let's begin the game...");
+    		System.out.println("Initial position is Square Zero");
+    		System.out.println("Position : Square " + POSITION);
+    	}
  }
-}
+
